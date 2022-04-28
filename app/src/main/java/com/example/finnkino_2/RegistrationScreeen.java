c2bc4c4e4e0437027d4e5b8629d97c8782d000f2
lauri <lauri.j.terho@gmail.com>
2022-04-28 13:55:30 +0300

    virheenkäsittelyä käyttäjätunnusten hallintaan

--- app/src/main/java/com/example/finnkino_2/RegistrationScreeen.java
+++ app/src/main/java/com/example/finnkino_2/RegistrationScreeen.java
@@ -2,6 +2,7 @@ package com.example.finnkino_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,17 +12,23 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class RegistrationScreeen extends AppCompatActivity {
 
-    EditText newuser;
-    EditText newpass;
+    EditText newUser;
+    EditText newPass;
     Button register;
     TextView login;
+    TextView errorMsg;
+    Boolean accountExists;
     Context context;
 
     @Override
@@ -31,29 +38,79 @@ public class RegistrationScreeen extends AppCompatActivity {
 
         context = RegistrationScreeen.this;
 
-        newuser = findViewById(R.id.editTextUserName);
-        newpass = findViewById(R.id.editTextPassword);
+        newUser = findViewById(R.id.editTextUserName);
+        newPass = findViewById(R.id.editTextPassword);
         register = findViewById(R.id.registerButton);
         login = findViewById(R.id.textLogin);
+        errorMsg = findViewById(R.id.textViewErrorMessage2);
+        errorMsg.setText("");
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try {
-                    OutputStreamWriter writer = new OutputStreamWriter(context.openFileOutput("accounts.txt", Context.MODE_APPEND));
-                    writer.write(newuser.getText().toString() + "," + newpass.getText().toString() + "\n");
-                    writer.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                checkAccounts();
             }
         });
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), LoginScreen.class);
-                startActivity(intent);
+                goToLogin();
             }
         });
+    }
+
+    public void saveAccount() {
+        try {
+            OutputStreamWriter writer = new OutputStreamWriter(context.openFileOutput("accounts.txt", Context.MODE_APPEND));
+            writer.write(newUser.getText().toString() + "," + newPass.getText().toString() + "\n");
+            writer.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void checkAccounts() {
+        try {
+            accountExists = false;
+            errorMsg.setText("");
+            InputStream inS = context.openFileInput("accounts.txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(inS));
+
+            String line;
+            String[] acc;
+            while((line = br.readLine())!=null) {
+                acc = line.split(",");
+
+                // go to main screen if the account exists
+                if (acc[0].equals(newUser.getText().toString())) {
+                    accountExists = true;
+                    errorMsg.setText("Käyttäjätunnus on jo olemassa");
+                    break;
+                }
+            }
+            inS.close();
+            br.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (!accountExists) {
+            saveAccount();
+            goToMain();
+        }
+    }
+
+    public void goToMain() {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToLogin() {
+        Intent intent = new Intent(getApplicationContext(), LoginScreen.class);
+        startActivity(intent);
     }
 }
